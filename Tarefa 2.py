922bbff9cdbda0d840f79db1f5060e9429f565b1
Igor Cirne <igor.oliveira.100@ufrn.edu.br>
2023-10-10 00:24:40 -0300

    Pequenas alterações

--- Tarefa 2.py
+++ Tarefa 2.py
@@ -19,11 +19,19 @@ Functions.Top_50(setups, setups_filtered)
 
 Functions.add_nodes_from_parts(G)
 
-#Criando os nodes dos setups baseados no índice deles
+# Criando os nodes dos setups baseados no índice deles
 
 Functions.add_edges(G,setups_filtered)
 
-nx.draw(G, with_labels=True)
+# Calculando Centralidade de cada nó
+degree_centrality = nx.degree_centrality(G)
+
+# Calcular posições dos nós usando spring layout
+positions = nx.spring_layout(G, k=0.3)
+
+# Desenhando o Digrafo
+nx.draw(G, pos=positions, with_labels=True, node_size=500, node_color='lightblue', font_size=12, font_color='black')
+plt.axis('off')
 plt.show()
 
 
