89bfffed3385f979be7560819c5dafda8bdd0aff
Igor Cirne <igor.oliveira.100@ufrn.edu.br>
2023-10-13 14:03:17 -0300

    Tarefa 2 Finalizada

--- Tarefa2.py
+++ Tarefa2.py
@@ -14,7 +14,7 @@ Functions.Create_Setups(setups)
 for x in range(len(setups)):
     setups_teamscore.append(setups[x].teamscore)
 
-Functions.Top_50(setups, setups_filtered)
+Functions.Top_10(setups, setups_filtered)
 
 # Criando os nodes do grafo
 
@@ -28,16 +28,18 @@ Functions.add_nodes_from_parts(G)
 Functions.add_edges(G,setups_filtered)
 out_degrees = dict(G.out_degree())
 max_out_degree = max(out_degrees.values())
+node_sizes = [100 + 500 * out_degrees[node] / max_out_degree for node in G.nodes()]
 edge_widths = [1.5 * out_degrees[edge[0]] / max_out_degree for edge in G.edges()]
 
 # Calcular posições dos nós usando circular layout
 
 positions = nx.circular_layout(G, scale=20.0)
 
+
 # Desenhando o Digrafo
 
 colors = ["lightblue" if n[1]['part'] else "red" for n in G.nodes(data=True)]
-nx.draw(G, pos=positions, width=edge_widths, with_labels=True, node_size=300, node_color=colors, font_size=12, font_color='black', font_weight='bold')
+nx.draw(G, pos=positions, with_labels=True, node_size=node_sizes, node_color=colors, font_size=12, font_color='black', font_weight='bold', verticalalignment='bottom')
 plt.show()
 
 
